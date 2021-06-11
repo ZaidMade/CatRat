61c1341865d68771f8f59dcad619d5a57cdfea6f
Work <4460816+ZaidMade@users.noreply.github.com>
2021-06-11 14:52:41 -0700

    ✅🐞 Removed unnecessary usage of eval

--- src/objects.js
+++ src/objects.js
@@ -4,8 +4,9 @@ class Entity{
     this.x = _x;
     this.y = _y;
     this.type = eval('types.' + _type);
-    this.sprite = eval('sprites.' + _type);
-    this.bop = ((Math.random() >= 0.5) ? true : false);
+    this.type = types[_type];
+    this.sprite = sprites[_type];
+    this.bop = (Math.random() >= 0.5);
   }
 
 }
